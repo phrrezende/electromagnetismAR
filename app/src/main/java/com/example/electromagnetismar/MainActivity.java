f93583a9dce1ea09f3b9c1dea6ca6948e0211ba8
paulo.rezende <paulo.rezende@ifsuldeminas.edu.br>
2020-01-08 15:20:06 -0300

    create initializeInstance method and extend ARActivity class.

--- app/src/main/java/com/example/electromagnetismar/MainActivity.java
+++ app/src/main/java/com/example/electromagnetismar/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.electromagnetismar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity {
+import org.artoolkitx.arx.arxj.ARActivity;
+import org.artoolkitx.arx.arxj.assets.AssetHelper;
+import org.artoolkitx.arx.arxj.rendering.ARRenderer;
+
+public class MainActivity extends ARActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initializeInstance();
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    private void initializeInstance() {
+        // Unpack assets to cache directory so native library can read them.
+        // N.B.: If contents of assets folder changes, be sure to increment the
+        // versionCode integer in the modules build.gradle file.
+        AssetHelper assetHelper = new AssetHelper(getAssets());
+        assetHelper.cacheAssetFolder(this, "Data");
+        assetHelper.cacheAssetFolder(this, "cparam_cache");
+    }
+
+    @Override
+    protected ARRenderer supplyRenderer() {
+        return null;
+    }
+
+    @Override
+    protected FrameLayout supplyFrameLayout() {
+        return null;
     }
 }
